1c6f7ad1b31263f055ef7e69b440305126335668
Tomas <tomas.huzevka@outlook.com>
2020-02-01 17:44:51 -0500

    2/1/2020

--- sakira.sql
+++ sakira.sql
@@ -1,22 +1,25 @@
 -- 1. What query would you run to get all the customers inside city_id = 312? Your query should return customer first name, last name, email, and address.
 SELECT customer.first_name, customer.last_name, address.city_id 
 FROM customer 
-JOIN address ON customer_id = city_id 
+JOIN address ON customer.address_id = address.address_id
 WHERE city_id = 312;
 
--- 1. What query would you run to get all the customers inside city_id = 312? Your query should return customer first name, last name, email, and address.
+-- 2. What query would you run to get all comedy films? Your query should return film title, description, release year, rating, special features, and genre (category).
 SELECT film.title, film.description, film.release_year, film.rating, film.special_features, category.name 
-FROM film JOIN category ON category_id = film_id; 
+FROM film JOIN category ON film_category.category_id = category.category_id
+WHERE category.name = 'Comedy';
 
 -- 3. What query would you run to get all the films joined by actor_id=5? Your query should return the actor id, actor name, film title, description, and release year.
 SELECT actor.actor_id, actor.first_name, actor.last_name, film.title, film.description, film.release_year 
 FROM actor 
-JOIN film ON film_id=actor_id
+JOIN film ON film_actor.actor_id = actor.actor_id
+WHERE actor.actor_id = 5;
 
 -- 4. What query would you run to get all the customers in store_id = 1 and inside these cities (1, 42, 312 and 459)? Your query should return customer first name, last name, email, and address.
 SELECT customer.first_name, customer.last_name, customer.email, customer.store_id, address.city_id 
 FROM customer 
-JOIN address ON customer_id=address.address_id 
+JOIN address ON customer.address_id = address.address_id 
+JOIN city ON address.city_id = city.city_id
 WHERE address.city_id IN (1, 42, 312, 459) AND customer.store_id = 1
 
 -- 5. What query would you run to get all the films with a "rating = G" and "special feature = behind the scenes", joined by actor_id = 15? Your query should return the film title, description, release year, rating, and special feature. Hint: You may use LIKE function in getting the 'behind the scenes' part.
@@ -27,7 +30,8 @@ WHERE film.rating = 'G' AND film.special_features LIKE '%Behind the Scenes%'  AN
 
 -- 6. What query would you run to get all the actors that joined in the film_id = 369? Your query should return the film_id, title, actor_id, and actor_name.
 SELECT film_actor.actor_id, actor.first_name, actor.last_name, film.title, film.film_id 
-FROM film JOIN film_actor ON film.film_id = film_actor.film_id 
+FROM film 
+JOIN film_actor ON film.film_id = film_actor.film_id 
 JOIN actor ON film_actor.actor_id=actor.actor_id 
 WHERE film.film_id = 369
 
